2eb0b71742d2e19e3dde262eaf927c9e5c0e1cb7
12118482 <11800585@aluno.cotemig.com.br>
2023-03-03 19:24:22 -0300

    Initial commit

--- src/br/newtonpaiva/ui/Main.java
+++ src/br/newtonpaiva/ui/Main.java
@@ -8,7 +8,12 @@ public class Main {
         c.setNumero(10);
         c.setSaldo(100.0);
 
-        c.depositar(50.0);
+//        c.depositar(50.0);
+
+
+        Conta c2 = new Conta();
+        c2.setNumero(90);
+        c2.setSaldo(500.0);
 
         System.out.println(c.getSaldo());
     }
