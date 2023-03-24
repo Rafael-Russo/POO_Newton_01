09aba410f533408bb98ebf57a1cda94ab3445151
12118482 <11800585@aluno.cotemig.com.br>
2023-03-24 19:35:52 -0300

    feat: implementa Pessoa Fisica e juridica

---
+++ src/br/newtonpaiva/dominio/Pessoa.java
@@ -0,0 +1,40 @@
+package br.newtonpaiva.dominio;
+
+import java.util.Objects;
+
+public abstract class Pessoa {
+    private String nome;
+
+    private Conta conta;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return nome.equals(pessoa.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
+
+    public abstract void validarDocumento();
+}
